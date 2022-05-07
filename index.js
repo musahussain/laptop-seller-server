80aaa426837297682e286dc3e38b5eb2ea228d35
musahussain <mdmuslimhussain1@gmail.com>
2022-05-07 20:08:20 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -8,6 +9,38 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5gpxm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run() {
+    try{
+        await client.connect();
+        const productCollection = client.db('warehouse').collection('products');
+
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+
+            const products = await cursor.toArray();
+
+            res.send(products);
+        })
+    }
+    finally{
+        console.log("connected");
+    }
+}
+
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send("Server is running");
