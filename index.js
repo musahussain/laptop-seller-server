d1c5ef0cf8602a29f1efd57d90dc1f1a186ae0ae
musahussain <mdmuslimhussain1@gmail.com>
2022-05-09 20:53:02 +0600

    created delete api

--- index.js
+++ index.js
@@ -56,6 +56,14 @@ async function run() {
             const result = await productCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         });
+
+        // delete a user
+        app.delete('/product/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally{
         console.log("connected");
